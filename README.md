eef74846ec662cbd082a63aaefd14b9bcb19a5ac
USOLVED <git@usolved.net>
2015-07-17 11:28:51 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,122 @@
+# check_usolved_barracuda_temperature
+
+## Overview
+
+This Python Nagios/Icinga plugin checks the temperature and fan state of Barracuda firewalls.
+If the temperature is higher than the given treshold you'll be informed.
+You'll also get noticed if a fan isn't working anymore with the integrated hardware check.
+
+The plugin also returns performance data for the temperature sensors.
+
+## Authors
+
+Ricardo Klement (www.usolved.net)
+
+## Installation
+
+Just copy the file check_usolved_barracuda_temperature.py into your Nagios plugin directory.
+For example this path: /usr/local/nagios/libexec/
+
+Give check_usolved_barracuda_temperature.py execution rights for the nagios user.
+This plugin needs Python 2 to be installed and uses the libraries sys, os and optparse.
+
+Why not Python 3 you may ask?
+Most Nagios / Icinga installations are already using other plugins which are written in Python 2.
+So for compatibility reasons I've decided to use Python 2 as well.
+
+Make sure you've enabled the SNMP service on your Barracuda firewall. If you have a cluster it's good to 
+configure the SNMP service on the virtual server layer on your Barracuda.
+Details to find [here](https://techlib.barracuda.com/display/BNGv54/How+to+Configure+the+SNMP+Service).
+
+I've tested the plugin on Barracuda appliances F100b, F200b, F200c, F600c and F800b.
+
+## Usage
+
+### Test on command line
+If you are in the Nagios plugin directory execute this command:
+
+<pre><code>
+./check_usolved_barracuda_temperature.py -H ip_address_of_barracuda -C snmp_community
+</code></pre>
+
+The output could be something like this:
+
+<pre><code>
+OK - 8 fans / CPU Temperature: 49.5 *C, Mainboard Temperature: 23.0 *C
+Chassis Fan 1A: 1917 RPM
+Chassis Fan 1B: 2109 RPM
+Chassis Fan 2A: 2343 RPM
+Chassis Fan 2B: 2812 RPM
+Chassis Fan 3A: 2481 RPM
+Chassis Fan 3B: 2721 RPM
+Chassis Fan 4A: 2481 RPM
+Chassis Fan 4B: 2909 RPM
+CPU Temperature: 49.5 *C
+Mainboard Temperature: 23.0 *C
+</code></pre>
+
+Here are all arguments that can be used within this plugin:
+
+<pre><code>
+-H &lt;host address&gt;
+Required: IP or hostname of the Barracuda firewall node with a running snmp service
+
+[-C &lt;snmp community&gt;]
+Optional: SNMP Community String
+
+[-V &lt;snmp version&gt;]
+Optional: SNMP version 1 or 2c are supported, if argument not given version 2 is used by default
+
+[-O &lt;object&gt;]
+Optional: temp or fan. If no object is given fans and temperature will be checked
+
+[-w &lt;warning&gt;]
+Optional: Warning threshold for the temperature in degree celsius
+
+[-c &lt;critical&gt;]
+Optional: Critical threshold for the temperature in degree celsius
+
+[-P &lt;perfdata&gt;]
+Optional: Write -P yes if you like to have performance data
+
+[-T &lt;timeout&gt;]
+Optional: SNMP timeout in seconds. Default is 30 seconds.
+</code></pre>
+
+### Install in Nagios
+
+Edit your **commands.cfg** and add the following.
+
+Example for checking the temperature with performande data enabled (detecting not working fans works by default):
+
+<pre><code>
+define command {
+    command_name    check_usolved_barracuda_temperature
+    command_line    $USER1$/check_usolved_barracuda_temperature.py -H $HOSTADDRESS$ -P yes -C public -w $ARG1$ -c $ARG2$
+}
+</code></pre>
+
+Example for just checking the fans (if they are not spinning anymore a critical will be thrown):
+
+<pre><code>
+define command {
+    command_name    check_usolved_barracuda_temperature
+    command_line    $USER1$/check_usolved_barracuda_temperature.py -H $HOSTADDRESS$ -P yes -C public -O fan
+}
+</code></pre>
+
+Edit your **services.cfg** and add the following.
+
+Example for checking the temperature:
+
+<pre><code>
+define service{
+	host_name				Test-Server
+	service_description		Barracuda-Temperature
+	use						generic-service
+	check_command			check_usolved_barracuda_temperature!60!70
+}
+</code></pre>
+
+
+You could also use host macros for the snmp community.
---
+++ check_usolved_barracuda_temperature.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python
+
+'''
+This Python Nagios/Icinga plugin checks the temperature and fan state of Barracuda firewalls.
+
+Python 2 is required with use of the libraries sys, os, optparse
+
+Copyright (c) 2015 www.usolved.net 
+Published under https://github.com/usolved/check_usolved_barracuda_temperature
+
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+------------------------
+
+v1.0 2015-07-17
+Initial release
+
+'''
+
+######################################################################
+# Import modules
+
+import sys
+import os
+import optparse
+
+
+######################################################################
+# Definitions of variables
+
+# Arrays for return codes and return message
+return_code 		= { 'OK': 0, 'WARNING': 1, 'CRITICAL': 2, 'UNKNOWN': 3 }
+return_msg 			= ''
+return_perfdata 	= ''
+return_status 		= return_code['UNKNOWN']
+
+
+# OIDs
+oid	= {
+	'Sensors':	'.1.3.6.1.4.1.10704.1.4.1',
+	'Name':		'.1',
+	'Type':		'.2',
+	'Value':	'.3'
+	}
+
+cmd = {
+	'SNMP_Walk': '/usr/bin/snmpwalk',
+	'SNMP_Get': '/usr/bin/snmpget'
+}
+
+######################################################################
+# Parse Arguments
+
+parser 		= optparse.OptionParser()
+parser.add_option('-H', '--hostname', 	help='Required: IP or hostname of the Barracuda firewall node with a running snmp service', dest='arg_hostname', type='string')
+parser.add_option('-C', '--community', 	help='Optional: SNMP Community String', dest='arg_snmp_community', type='string', default='public')
+parser.add_option('-V', '--version', 	help='Optional: SNMP version 1 or 2c are supported, if argument not given version 2 is used by default', dest='arg_snmp_version', type='string', default='2c')
+parser.add_option('-O', '--object', 	help='Optional: temp or fan. If no object is given fans and temperature will be checked', dest='arg_object', type='string', default='all')
+parser.add_option('-w', '--warning', 	help='Optional: Warning threshold for the temperature in degree celsius', dest='arg_warning', type='string')
+parser.add_option('-c', '--critical', 	help='Optional: Critical threshold for the temperature in degree celsius', dest='arg_critical', type='string')
+parser.add_option('-P', '--perfdata', 	help='Optional: Write -P yes if you like to have performance data', dest='arg_perfdata', type='string')
+parser.add_option('-T', '--timeout', 	help='Optional: SNMP timeout in seconds', dest='arg_timeout', type='int', default=30)
+(opts, args) = parser.parse_args()
+
+arg_hostname 		= opts.arg_hostname
+arg_snmp_community	= opts.arg_snmp_community
+arg_snmp_version	= opts.arg_snmp_version
+arg_object			= opts.arg_object
+arg_warning			= opts.arg_warning
+arg_critical		= opts.arg_critical
+arg_perfdata		= opts.arg_perfdata
+arg_timeout			= opts.arg_timeout
+
+
+
+######################################################################
+# Functions
+
+def output_nagios(return_msg, return_perfdata, return_code):
+
+	print return_msg+return_perfdata
+
+	sys.exit(return_code)
+
+
+
+def get_sensors_execute(cmdline):
+
+	sensor 				= []
+	cmdline_return 		= os.popen(cmdline)
+
+
+	for line in cmdline_return.readlines():
+		sensor.append(line.rstrip())
+
+	cmdline_return_code = cmdline_return.close()
+	return sensor
+
+
+
+def get_sensors():
+
+	cmdline_snmp 		= cmd['SNMP_Walk']+' -v '+arg_snmp_version+' -c '+arg_snmp_community+' -OqevU -t '+ str(arg_timeout) +' '+arg_hostname
+	cmdline_oid_name 	= oid['Sensors']+oid['Name']
+	cmdline_oid_type 	= oid['Sensors']+oid['Type']
+	cmdline_oid_value 	= oid['Sensors']+oid['Value']
+
+	# read snmp info for every sensor
+	sensor_name 		= get_sensors_execute(cmdline_snmp+' '+cmdline_oid_name)
+	sensor_type 		= get_sensors_execute(cmdline_snmp+' '+cmdline_oid_type)
+	sensor_value 		= get_sensors_execute(cmdline_snmp+' '+cmdline_oid_value)
+
+
+	# put the returned data into a dictionary to have the data in context
+	sensors 	= []
+
+	i = 0
+	while i < len(sensor_name):
+
+		tmp_dict 	= {'name': sensor_name[i], 'type': sensor_type[i], 'value': sensor_value[i]}
+
+		sensors.append(tmp_dict)
+
+		i += 1
+
+
+	return sensors
+
+
+
+def check_sensors(sensors):
+
+	global return_msg, return_perfdata
+
+	return_key 				= "OK"
+	return_msg_put 			= ""
+	return_msg_put_temp 	= ""
+	return_perfdata_put 	= ""
+	return_msg_extended_put = ""
+
+	count_temp 				= 0
+	count_fan 				= 0
+
+
+	for sensor_data in sensors:
+
+		# the value from snmp is like 35000 so we convert it to a float when the current sensor is temperature
+
+		if sensor_data['type'] == '2':
+			sensor_value_float = float(sensor_data['value']) / 1000
+		else:
+			sensor_value_float = 0.0
+
+
+		# check if a sensor is in a critical state
+
+		if (arg_object == 'all' or arg_object == 'fan') and sensor_data['type'] == '1' and int(sensor_data['value']) == 0:
+
+			return_msg_put += sensor_data['name'] + " is not working, "
+			return_key = 'CRITICAL'
+
+		elif (arg_object == 'all' or arg_object == 'temp') and sensor_data['type'] == '2' and arg_critical and sensor_value_float >= float(arg_critical):
+
+			return_msg_put += sensor_data['name'] + " is " + str(sensor_value_float) + " *C, "
+			return_key = 'CRITICAL'
+
+		elif (arg_object == 'all' or arg_object == 'temp') and sensor_data['type'] == '2' and arg_warning and sensor_value_float >= float(arg_warning):
+
+			return_msg_put += sensor_data['name'] + " is " + str(sensor_value_float) + " *C, "
+
+			if return_key != 'CRITICAL':
+				return_key = 'WARNING'
+
+
+		# count the found sensors for the output
+
+		if (arg_object == 'all' or arg_object == 'fan') and sensor_data['type'] == '1':
+			count_fan += 1
+
+			# add extended output for fans
+			return_msg_extended_put += "\n" + sensor_data['name'] + ": " + sensor_data['value'] + " RPM"
+
+		elif (arg_object == 'all' or arg_object == 'temp') and sensor_data['type'] == '2':
+			count_temp += 1
+
+			# add extended output for temperature
+			return_msg_extended_put += "\n" + sensor_data['name'] + ": " + str(sensor_value_float) + " *C"
+			return_msg_put_temp 	+= sensor_data['name'] + ": " + str(sensor_value_float) + " *C, "
+
+			if arg_perfdata == 'yes':
+				return_perfdata_put += sensor_data['name'] + '=' + str(sensor_value_float) + ', '
+
+
+	# put together the output message
+
+	if return_key == 'OK':
+
+		sensor_output 		= ''
+
+		if count_fan > 0:
+
+			if count_fan > 1:
+				label_fan = "fans"
+			else:
+				label_fan = "fan"
+
+			sensor_output += str(count_fan) + " " + label_fan + " / "
+
+		if count_temp > 0:
+			sensor_output += return_msg_put_temp[:-2] + " / "
+
+
+		sensor_output = sensor_output[:-3]
+
+		#if return_msg_put_temp:
+			#sensor_output += " - " + return_msg_put_temp[:-2]
+
+
+		# if sensor_output is not empty then sensors where found
+
+		if sensor_output:
+			return_msg = return_key + " - " + sensor_output
+
+		else:
+			return_key = 'UNKNOWN'
+			return_msg = return_key + " - No sensors found"
+
+
+	else:
+		return_msg = return_key + " - " + return_msg_put[:-2]
+
+
+
+	# extended output
+
+	if return_msg_extended_put:
+		return_msg += return_msg_extended_put
+
+
+
+	# if perfdata found
+
+	if return_perfdata_put:
+		return_perfdata = return_perfdata_put[:-2]
+
+
+	return return_code[return_key]
+
+
+
+
+######################################################################
+# General
+
+if not arg_hostname:
+
+	return_msg = 'UNKNOWN - Not all required arguments given\nType ./'+os.path.basename(__file__)+' --help for all options.'
+
+	output_nagios(return_msg, '', return_code['UNKNOWN'])
+
+else:
+
+	sensors 		= get_sensors()
+
+	return_status 	= check_sensors(sensors)
+
+
+
+	# If performace data available, build output string
+	if arg_perfdata == 'yes' and return_perfdata:
+		return_perfdata = ' | ' + return_perfdata
+
+
+	# output the string to std
+	output_nagios(return_msg, return_perfdata, return_status)
